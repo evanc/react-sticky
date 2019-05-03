e5ade3bd7edb0df57a0b478c13d7f28349c4b93e
evanc <evan@coonrod.org>
2019-05-03 10:01:13 -0700

    Stop using React.PropTypes

--- src/container.js
+++ src/container.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
-import Channel from './channel';
+import Channel from "./channel";
 
 export default class Container extends React.Component {
-
   static contextTypes = {
-    'sticky-channel': React.PropTypes.any,
-  }
+    "sticky-channel": PropTypes.any
+  };
 
   static childContextTypes = {
-    'sticky-channel': React.PropTypes.any,
-  }
+    "sticky-channel": PropTypes.any
+  };
 
   constructor(props) {
     super(props);
@@ -19,33 +19,37 @@ export default class Container extends React.Component {
   }
 
   getChildContext() {
-    return { 'sticky-channel': this.channel };
+    return { "sticky-channel": this.channel };
   }
 
   componentWillMount() {
-    const parentChannel = this.context['sticky-channel'];
+    const parentChannel = this.context["sticky-channel"];
     if (parentChannel) parentChannel.subscribe(this.updateOffset);
   }
 
   componentDidMount() {
     const node = ReactDOM.findDOMNode(this);
-    this.channel.update((data) => { data.node = node });
+    this.channel.update(data => {
+      data.node = node;
+    });
   }
 
   componentWillUnmount() {
-    this.channel.update((data) => { data.node = null });
+    this.channel.update(data => {
+      data.node = null;
+    });
 
-    const parentChannel = this.context['sticky-channel'];
+    const parentChannel = this.context["sticky-channel"];
     if (parentChannel) parentChannel.unsubscribe(this.updateOffset);
   }
 
   updateOffset = ({ inherited, offset }) => {
-    this.channel.update((data) => { data.inherited = inherited + offset });
-  }
+    this.channel.update(data => {
+      data.inherited = inherited + offset;
+    });
+  };
 
   render() {
-    return <div {...this.props}>
-      {this.props.children}
-    </div>
+    return <div {...this.props}>{this.props.children}</div>;
   }
 }
